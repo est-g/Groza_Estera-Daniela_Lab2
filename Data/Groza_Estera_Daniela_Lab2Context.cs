9606dd348b3e155090af57afc816769082f75215
est-g <esttt.pc@gmail.com>
2023-11-02 20:30:33 +0200

    Add project files.

---
+++ Data/Groza_Estera_Daniela_Lab2Context.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Groza_Estera_Daniela_Lab2.Models;
+
+namespace Groza_Estera_Daniela_Lab2.Data
+{
+    public class Groza_Estera_Daniela_Lab2Context : DbContext
+    {
+        public Groza_Estera_Daniela_Lab2Context (DbContextOptions<Groza_Estera_Daniela_Lab2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Groza_Estera_Daniela_Lab2.Models.Book> Book { get; set; } = default!;
+
+        public DbSet<Groza_Estera_Daniela_Lab2.Models.Publisher>? Publisher { get; set; }
+    }
+}
---
+++ Models/Publisher.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+
+namespace Groza_Estera_Daniela_Lab2.Models
+{
+    public class Publisher
+    {
+        public int ID { get; set; }
+        public string PublisherName { get; set; }
+        public ICollection<Book>? Books { get; set; }
+    }
+}
